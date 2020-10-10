e5c59c2aa2a97b35daa3c7fa7d2725cd877fe34c
Riccardo Paltrinieri <61794499+riccardopaltrinieri@users.noreply.github.com>
2020-10-10 14:07:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# rickVideo
+rickVideo uses Spring and React frameworks to create a video-sharing website.
+
+## How to install 
+How to get it from git
+```sh
+$ git clone https://github.com/riccardopaltrinieri/video_webapp.git
+```
---
+++ src/test/java/com/rickpalt/rickvideo/RickvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickpalt.rickvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickvideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
